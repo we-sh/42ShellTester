00fa90ea62774ac24ef9c08bb549a939e77e935f
Jean-michel GIGAULT <jgigault@student.42.fr>
2016-02-12 13:16:06 +0100

    add specs

--- 42ShellChecker.sh
+++ 42ShellChecker.sh
@@ -3,6 +3,8 @@
 declare GLOBAL_PROG="$1"
 declare GLOBAL_INSTALLDIR="$(pwd)"
 declare GLOBAL_TMP_DIRECTORY="${GLOBAL_INSTALLDIR}/tmp"
+declare C_RED="\033[31m\033[38;5;160m"
+declare C_CLEAR="\033[0m"
 
 mkdir "${GLOBAL_TMP_DIRECTORY}" 2>/dev/null
 
@@ -10,9 +12,9 @@ function run_expected_to_be_empty
 {
   if [ "$(awk '{print}' "${RESPONSE}")" == "" ]
   then
-    printf "."
+    return 0
   else
-    printf "!"
+    return 1
   fi
 }
 
@@ -20,9 +22,19 @@ function run_expected_to_be_filled
 {
   if [ "$(awk '{print}' "${RESPONSE}")" != "" ]
   then
-    printf "."
+    return 0
   else
-    printf "!"
+    return 1
+  fi
+}
+
+function run_expected_to_have_nb_of_lines
+{
+  if [ "$(awk 'END {printf NR}' "${RESPONSE}")" == "${EXPECTED_TO_ARGS[0]}" ]
+  then
+    return 0
+  else
+    return 1
   fi
 }
 
@@ -30,9 +42,9 @@ function run_expected_to_have_not_regexp
 {
   if [ "$(awk -v regexp="${EXPECTED_TO_ARGS[0]}" '$0 ~ regexp {print}' "${RESPONSE}")" == "" ]
   then
-    printf "."
+    return 0
   else
-    printf "!"
+    printf 1
   fi
 }
 
@@ -40,9 +52,9 @@ function run_expected_to_have_regexp
 {
   if [ "$(awk -v regexp="${EXPECTED_TO_ARGS[0]}" '$0 ~ regexp {print}' "${RESPONSE}")" != "" ]
   then
-    printf "."
+    return 0
   else
-    printf "!"
+    return 1
   fi
 }
 
@@ -62,19 +74,19 @@ function run_expector
   local EXPECTED_STD="RESPONSE_${1}"
   local RESPONSE="${!EXPECTED_STD}"
   local TEST_CMD
+  local TEST_RETURN
   local OLD_IFS="${IFS}"
 
+  #printf "  ${1}\n"
   IFS=$'\n'
   for LINE in $(cat "${TEST}/$(echo "${1}" | awk '{print tolower($0)}')")
   do
     TEST_CMD="$(echo "${LINE}" | awk '{ print $1 }')"
-    case "${TEST_CMD}" in
-      "expected_to")
-        eval run_expected_to ${LINE}
-        ;;
-    esac
-    printf " %s" "${LINE}"
-    printf "\n"
+    eval "run_${TEST_CMD}" ${LINE}
+    if [ "${?}" != "0" ]
+    then
+      printf "${C_RED}  ${1} %s${C_CLEAR}\n" "${LINE}"
+    fi
   done
 
   IFS="${OLD_IFS}"
@@ -84,6 +96,7 @@ function run_specs
 {
   local TEST
   local TEST_NAME
+  local TEST_FULLNAME
   local RESULT
   local RESPONSE_STDOUT
   local RESPONSE_STDERR
@@ -93,8 +106,9 @@ function run_specs
   for TEST in $(find "${GLOBAL_INSTALLDIR}" -type d -regex "${GLOBAL_INSTALLDIR}/spec/.*/[0-9]\{3\}-.*")
   do
     TEST_NAME="${TEST##*/}"
+    TEST_FULLNAME="${TEST##*spec/}"
 
-    printf "\n#%s\n" "${TEST_NAME}"
+    printf "#%s\n" "${TEST_FULLNAME}"
 
     eval "${GLOBAL_PROG}" < "${TEST}/stdin" 1> "${GLOBAL_TMP_DIRECTORY}/spec.${TEST_NAME}.stdout" 2> "${GLOBAL_TMP_DIRECTORY}/spec.${TEST_NAME}.stderr"
 
