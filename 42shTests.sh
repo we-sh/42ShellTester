da520aad908188f271adc6438ba3740b35babd28
Gabriel KUMA <gkuma@e2r11p3.42.fr>
2016-02-19 19:42:08 +0100

    Test wrote for setenv, start unsetenv

--- 42shTests.sh
+++ 42shTests.sh
@@ -5,12 +5,13 @@ declare GLOBAL_SPECS_MATCHER="$2"
 declare GLOBAL_INSTALLDIR="$(pwd)"
 declare GLOBAL_TMP_DIRECTORY="${GLOBAL_INSTALLDIR}/tmp"
 declare C_RED="\033[31m\033[38;5;160m"
-declare C_GREEN="\033[31m\033[38;5;70m"
+declare C_GREEN="\033[31m\033[38;5;34m"
+declare C_YELLOW="\033[31m\033[1;33m"
 declare C_CLEAR="\033[0m"
 
 mkdir "${GLOBAL_TMP_DIRECTORY}" 2>/dev/null
 
-function run_expected_to_be_empty
+function run_verb_be_empty
 {
   if [ "$(awk '{print}' "${RESPONSE}")" == "" ]
   then
@@ -20,7 +21,7 @@ function run_expected_to_be_empty
   fi
 }
 
-function run_expected_to_be_filled
+function run_verb_be_filled
 {
   if [ "$(awk '{print}' "${RESPONSE}")" != "" ]
   then
@@ -30,7 +31,7 @@ function run_expected_to_be_filled
   fi
 }
 
-function run_expected_to_have_nb_of_lines
+function run_verb_have_nb_of_lines
 {
   if [ "$(awk 'END {printf NR}' "${RESPONSE}")" == "${EXPECTED_TO_ARGS[0]}" ]
   then
@@ -40,7 +41,7 @@ function run_expected_to_have_nb_of_lines
   fi
 }
 
-function run_expected_to_have_not_regexp
+function run_verb_have_not_regexp
 {
   if [ "$(awk -v regexp="${EXPECTED_TO_ARGS[0]}" '$0 ~ regexp {print}' "${RESPONSE}")" == "" ]
   then
@@ -50,13 +51,59 @@ function run_expected_to_have_not_regexp
   fi
 }
 
-function run_expected_to_have_regexp
+function run_verb_have_regexp
 {
   if [ "$(awk -v regexp="${EXPECTED_TO_ARGS[0]}" '$0 ~ regexp {print}' "${RESPONSE}")" != "" ]
   then
     return 0
   else
     return 1
+  fi
+}
+
+function run_verb_match_each_lines_of_file
+{
+  INDEX=0
+  ERROR=0
+  TOTAL=$(awk 'END {print NR+1}' "${EXPECTED_TO_ARGS[0]}")
+  while [ "$INDEX" -le "$TOTAL" ]
+  do
+    LINE="$(awk -v INDEX="${INDEX}" 'NR == INDEX {print; exit}' "${EXPECTED_TO_ARGS[0]}")"
+    if [ "$(awk -v regexp="${LINE}" '$0 ~ regexp {print}' "${RESPONSE}")" == "" ]
+    then
+      ERROR=1
+      printf "${C_RED}  ${1} %s${C_CLEAR}\n" "${LINE}"
+    fi
+    (( INDEX += 1 ))
+  done
+  if [ ${ERROR} -eq 1 ]
+  then
+    return 1
+  else
+    return 0
+  fi
+}
+
+function run_verb_special_funct_match_parent_env
+{
+  INDEX=0
+  ERROR=0
+  TOTAL=$(awk 'END {print NR+1}' "${EXPECTED_TO_ARGS[0]}")
+  while [ "$INDEX" -le "$TOTAL" ]
+  do
+    VARIABLE="$(awk -v INDEX="${INDEX}" 'BEGIN {FS="="} NR == INDEX {print $1; exit}' "${EXPECTED_TO_ARGS[0]}")"
+    if [ "$(awk -v regexp="${VARIABLE}\=" '$0 ~ regexp {print}' "${RESPONSE}")" == "" ]
+    then
+      ERROR=1
+      printf "${C_RED}  ${1} %s${C_CLEAR}\n" "${VARIABLE}"
+    fi
+    (( INDEX += 1 ))
+  done
+  if [ ${ERROR} -eq 1 ]
+  then
+    return 1
+  else
+    return 0
   fi
 }
 
@@ -67,7 +114,29 @@ function run_expected_to
   shift 1
   local -a EXPECTED_TO_ARGS=${@}
 
-  eval "run_expected_to_${EXPECTED_TO_CMD}"
+  eval "run_verb_${EXPECTED_TO_CMD}"
+  if [ "${?}" != "0" ]
+  then
+    printf "${C_RED}  ✗ ${1} %s${C_CLEAR}\n" "${LINE}"
+  else
+    printf "${C_GREEN}  √ ${1} %s${C_CLEAR}\n" "${LINE}"
+  fi
+}
+
+function run_might
+{
+  shift 1
+  local EXPECTED_TO_CMD="${1}"
+  shift 1
+  local -a EXPECTED_TO_ARGS=${@}
+
+  eval "run_verb_${EXPECTED_TO_CMD}"
+  if [ "${?}" != "0" ]
+  then
+    printf "${C_YELLOW}  ~ ${1} %s${C_CLEAR}\n" "${LINE}"
+  else
+    printf "${C_GREEN}  √ ${1} %s${C_CLEAR}\n" "${LINE}"
+  fi
 }
 
 function run_expector
@@ -85,12 +154,6 @@ function run_expector
   do
     TEST_CMD="$(echo "${LINE}" | awk '{ print $1 }')"
     eval "run_${TEST_CMD}" ${LINE}
-    if [ "${?}" != "0" ]
-    then
-      printf "${C_RED}  ${1} %s${C_CLEAR}\n" "${LINE}"
-    else
-      printf "${C_GREEN}  ${1} %s ok ${C_CLEAR}\n" "${LINE}"
-    fi
   done
 
   IFS="${OLD_IFS}"
@@ -114,6 +177,11 @@ function run_specs
     TEST_FULLNAME="${TEST##*spec/}"
 
     printf "#%s\n" "${TEST_FULLNAME}"
+
+    if [ -f "${TEST}/before_exec" ]
+    then
+      eval "zsh" "${TEST}/before_exec"
+    fi
 
     eval "${GLOBAL_PROG}" < "${TEST}/stdin" 1> "${GLOBAL_TMP_DIRECTORY}/spec.${TEST_NAME}.stdout.raw" 2> "${GLOBAL_TMP_DIRECTORY}/spec.${TEST_NAME}.stderr.raw"
 
