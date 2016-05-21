dea1439ca7e76858c8b6ee539f1db2ee0427bd17
Jean-Michel Gigault <jgigault@student.42.fr>
2016-05-21 11:58:52 +0200

    refactor tests and add one test for pipe

---
+++ support/sleep-and-write-on-stderr/main.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+  int seconds = 1;
+  if (argc > 1)
+    seconds = atoi(argv[1]);
+  sleep(seconds);
+  if (argc > 2)
+  {
+    write(2, argv[2], strlen(argv[2]));
+  }
+  return (0);
+}
